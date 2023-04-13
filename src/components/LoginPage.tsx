80d05546e2a287efc8de81c9ed791c2a4a51e7ed
dver26 <fercorvictor@gmail.com>
2023-04-14 00:13:31 +0200

    added supabase sign up

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { supabase } from '../supabase/supabase'
 
 const LoginPage: React.FC = () => {
-  return <div>LoginPage</div>
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleUsernameChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setEmail(e?.target.value)
+  }
+
+  const handlePasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setPassword(e?.target.value)
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault()
+    try {
+      const res = await supabase.auth.signUp({
+        email,
+        password
+      })
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <form action='' onSubmit={handleSubmit}>
+      <label htmlFor=''>Username</label>
+      <input type='text' onChange={handleUsernameChange} />
+      <br />
+      <label htmlFor=''>Password</label>
+      <input type='text' onChange={handlePasswordChange} />
+      <button type='submit'>Send</button>
+    </form>
+  )
 }
 
 export default LoginPage
